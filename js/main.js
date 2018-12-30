85469213f7b91f3a9dd79a565795fd44f5750b7e
emillaii <laikwoktai@gmail.com>
2018-12-30 19:50:02 +0800

    Update the flowchart

--- js/main.js
+++ js/main.js
@@ -1,22 +1,37 @@
 $(document).ready(function() {
+	
     var data = {};
-    var init_aa_params = { mode: 1, start_pos: 2, stop_pos: 3 };
+    var init_aa_params = { mode: 1, start_pos: 2, stop_pos: 3,
+						   offset_in_um: 0, delay_Z_in_ms: 200,
+						   wait_tilt: 1, edge_filter: 0, is_debug: 0};
     var init_oc_params = { debug: false };
-    var init_basic_params = { retry: 3 , delay_in_ms: 100 };
+	var init_uv_params = { time: 3000 };
+	var init_initial_tilt_params = { roll: 0, pitch: 0};
+    var init_basic_params = { retry: 0 , delay_in_ms: 0 };
 
     var latestCreatedLinkId;
-    var $operatorProperties = $('#operator_properties');
     var $linkProperties = $('#link_properties');
     var $operatorTitle = $('#operator_title');
     var $aa_operatorTitle = $('#aa_operator_title');
+	var $initial_tilt_operatorTitle = $('#initial_tilt_operator_title');
     var $operator_properties = $('#operator_properties');
     var $aa_operator_properties = $('#aa_operator_properties');
+	var $initial_tilt_properties = $('#initial_tilt_properties');
     var $linkColor = $('#link_color');
 
     //Init the table 
     $aa_operator_properties.append("<div style=\"margin-top:20px\">Select AA mode: <select id=\"aa_mode\"><option value=0>DOV_Search</option><option value=1>ZScan</option></select></div>");
     $aa_operator_properties.append("<div style=\"margin-top:20px\">Start Position: <input type=\"number\" id=\"aa_start_position\"></div>");
     $aa_operator_properties.append("<div style=\"margin-top:20px\">Stop Position: <input type=\"number\" id=\"aa_stop_position\"></div>");
+	$aa_operator_properties.append("<div style=\"margin-top:20px\">Step Size: <input type=\"number\" id=\"aa_step_size\"></div>");
+	$aa_operator_properties.append("<div style=\"margin-top:20px\">AA Offset in um: <input type=\"number\" id=\"aa_offset_in_um\"></div>");
+	$aa_operator_properties.append("<div style=\"margin-top:20px\">Delay in ms: <input type=\"number\" id=\"aa_delay_Z_in_ms\"></div>");
+	$aa_operator_properties.append("<div style=\"margin-top:20px\">Wait tilt: <select id=\"aa_wait_tilt\"><option value=0>False</option><option value=1>True</option></select></div>");
+	$aa_operator_properties.append("<div style=\"margin-top:20px\">Edge Filter: <select id=\"aa_edge_filter\"><option value=0>No Edge Filter</option><option value=1>Vertical Edge Filter</option><option value=2>Horizontal Edge Filter</option></select></div>");
+	$aa_operator_properties.append("<div style=\"margin-top:20px\">Debug: <select id=\"aa_is_debug\"><option value=0>False</option><option value=1>True</option></select></div>");
+	
+	$initial_tilt_properties.append("<div style=\"margin-top:20px\">Roll: <input type=\"number\" id=\"roll\"></div>");
+    $initial_tilt_properties.append("<div style=\"margin-top:20px\">Pitch: <input type=\"number\" id=\"pitch\"></div>");
 
     $operator_properties.append("<div style=\"margin-top:20px\">Retry Count: <input type=\"number\" id=\"basic_retry\"></div>");
     $operator_properties.append("<div style=\"margin-top:20px\">Delay: <input type=\"number\" id=\"basic_delay\"></div>");
@@ -31,16 +46,30 @@ $(document).ready(function() {
       linkWidth: 4,
       onOperatorSelect: function(operatorId) {
         var params = $flowchart.flowchart('getOperatorParams', operatorId);
-        if (operatorId.includes("AA_")) {
-          $operatorProperties.hide();
+        $operator_properties.hide();
+        $aa_operator_properties.hide();
+		$initial_tilt_properties.hide();
+		
+		if (operatorId.includes("AA_")) {
           $aa_operator_properties.show();
           $aa_operatorTitle.val($flowchart.flowchart('getOperatorTitle', operatorId));
           $('#aa_mode').val(params["mode"]);
           $('#aa_start_position').val(params["start_pos"]);
           $('#aa_stop_position').val(params["stop_pos"]);
-        } else {
-          $aa_operator_properties.hide();
-          $operatorProperties.show();
+		  $('#aa_step_size').val(params["step_size"]);
+		  $('#aa_offset_in_um').val(params["offset_in_um"]); 
+		  $('#aa_delay_Z_in_ms').val(params["delay_Z_in_ms"]);
+		  $('#aa_wait_tilt').val(params["wait_tilt"]);
+		  $('#aa_edge_filter').val(params["edge_filter"]);
+		  $('#aa_is_debug').val(params["is_debug"]);
+        } else if (operatorId.includes("Initial Tilt")) {
+          $initial_tilt_properties.show();
+		  $initial_tilt_operatorTitle.val($flowchart.flowchart('getOperatorTitle', operatorId));
+          $('#roll').val(params["roll"]);
+          $('#pitch').val(params['pitch']);
+        }			
+		else {
+          $operator_properties.show();
           $operatorTitle.val($flowchart.flowchart('getOperatorTitle', operatorId));
           $('#basic_delay').val(params["delay_in_ms"]);
           $('#basic_retry').val(params['retry']);
@@ -48,8 +77,9 @@ $(document).ready(function() {
         return true;
       },
       onOperatorUnselect: function() {
-        $operatorProperties.hide();
+        $operator_properties.hide();
         $aa_operator_properties.hide();
+		$initial_tilt_properties.hide();
         return true;
       },
       onLinkSelect: function(linkId) {
@@ -124,7 +154,9 @@ $(document).ready(function() {
       };
       if (operatorId.includes("AA_")) {
         $flowchart.flowchart('setOperatorParams', operatorId, init_aa_params);
-      } else {
+      } else if (operatorId.includes("Initial Tilt")) {
+		$flowchart.flowchart('setOperatorParams', operatorId, init_initial_tilt_params);
+	  } else {
         $flowchart.flowchart('setOperatorParams', operatorId, init_basic_params);
       }
     }
@@ -174,13 +206,23 @@ $(document).ready(function() {
       addOperationWidget("test");
     });
 
+	$flowchart.siblings('.create_load_camera').click(function() { addOperationWidget("Load Camera"); });
     $flowchart.siblings('.create_init_camera').click(function() { addOperationWidget("Init Camera"); });
+    $flowchart.siblings('.create_pr_to_bond').click(function() { addOperationWidget("PR To Bond"); });
+	$flowchart.siblings('.create_initial_tilt').click(function() { addOperationWidget("Initial Tilt"); });
+    $flowchart.siblings('.create_aa_pick_lens').click(function() { addOperationWidget("AA Pick Lens"); });
+    
     $flowchart.siblings('.create_aa').click(function() { addOperationWidget("AA"); });
     $flowchart.siblings('.create_oc').click(function() { addOperationWidget("OC"); });
+	$flowchart.siblings('.create_mtf').click(function() { addOperationWidget("MTF"); });
+    
     $flowchart.siblings('.create_disp').click(function() { addOperationWidget("Dispense"); });
     $flowchart.siblings('.create_uv').click(function() { addOperationWidget("UV"); });
+	$flowchart.siblings('.create_delay').click(function() { addOperationWidget("Delay"); });
+    
     $flowchart.siblings('.create_accept').click(function() { addEndWidget("Accept"); });
     $flowchart.siblings('.create_reject').click(function() { addEndWidget("Reject"); });
+    $flowchart.siblings('.create_terminate').click(function() { addEndWidget("Terminate"); });
     
     $flowchart.siblings('.delete_selected_button').click(function() { $flowchart.flowchart('deleteSelected'); });
     
@@ -199,13 +241,20 @@ $(document).ready(function() {
        if (selectedOperatorId == null) { return; }
        console.log('update opeator: ' + selectedOperatorId);
        if (selectedOperatorId.includes("AA_")) {
-        $flowchart.flowchart('setOperatorTitle', selectedOperatorId, $aa_operatorTitle.val());
-        var params = { mode: $('#aa_mode').val(), start_pos: $('#aa_start_position').val(), stop_pos: $('#aa_stop_position').val() };
-        $flowchart.flowchart('setOperatorParams', selectedOperatorId, params);
-       } else {
-         $flowchart.flowchart('setOperatorParams', selectedOperatorId, init_basic_params);
-         var params = { retry: $('#basic_retry').val(), delay_in_ms: $('#basic_delay').val() };
-         $flowchart.flowchart('setOperatorParams', selectedOperatorId, params);
+			$flowchart.flowchart('setOperatorTitle', selectedOperatorId, $aa_operatorTitle.val());
+			var params = { mode: $('#aa_mode').val(), start_pos: $('#aa_start_position').val(), stop_pos: $('#aa_stop_position').val(), step_size: $('#aa_step_size').val(), offset_in_um: $('#aa_offset_in_um').val(), delay_Z_in_ms: $('#aa_delay_Z_in_ms').val(),
+			wait_tilt: $('#aa_wait_tilt').val(), edge_filter: $('#aa_edge_filter').val(), is_debug: $('#aa_is_debug').val() };
+			
+			$flowchart.flowchart('setOperatorParams', selectedOperatorId, params);
+       } else if (selectedOperatorId.includes("Initial Tilt")) {
+			$flowchart.flowchart('setOperatorParams', selectedOperatorId, $initial_tilt_operatorTitle.val());
+			var params = { roll: $('#roll').val(), pitch: $('#pitch').val() };
+			$flowchart.flowchart('setOperatorParams', selectedOperatorId, params);
+	   }		   
+	   else {
+			$flowchart.flowchart('setOperatorParams', selectedOperatorId, init_basic_params);
+			var params = { retry: $('#basic_retry').val(), delay_in_ms: $('#basic_delay').val() };
+			$flowchart.flowchart('setOperatorParams', selectedOperatorId, params);
        }
        alert("Params is updated");
     });
